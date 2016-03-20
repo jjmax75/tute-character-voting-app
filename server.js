2e427a3cb9d2cf2c6955d06ca0a7cc3b8208c449
John Behan <behanj@gmail.com>
2016-03-20 13:09:09 +0700

    Basic app with routes all setup now
    Boilerplate candidate

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// Babel ES6/JSX compiler
+require('babel-register');
+
+var swig = require('swig');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var routes = require('./app/routes');
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -12,6 +21,22 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (redirectLocation) {
+      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search);
+    } else if (renderProps) {
+      var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+      var page = swig.renderFile('views/index.html', { html: html });
+      res.status(200).send(page);
+    } else {
+      res.status(404).send('Page Not Found');
+    }
+  });
+});
 
 app.listen(port, function() {
   console.log('Node.js listening on port ' + port + '...');
